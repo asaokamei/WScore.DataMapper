be4a6b2b3628182faa734f8ddc68ca8311509f6a
Asao <kamei@workspot.jp>
2013-03-07 06:10:38 +0900

    make forms in each of the Element object.

--- src/WScore/Selector/Element/Text.php
+++ src/WScore/Selector/Element/Text.php
@@ -4,11 +4,26 @@ namespace WScore\Selector;
 class Element_Text extends Selector
 {
     /**
-     * @param Form $form
+     * @param \WScore\Html\Forms $form
      */
     public function __construct( $form )
     {
         parent::__construct( $form );
         $this->style = 'text';
+    }
+    /**
+     * make FORM type of value.
+     * create HTML Form element based on style.
+     *
+     * @param $value
+     * @return \WScore\Html\Elements
+     */
+    public function makeForm( $value )
+    {
+        $input = $this->form->input( $this->style, $this->name, $value, $this->attributes );
+        if( empty( $this->item_data ) ) { 
+            return $input; 
+        }
+        return $input;
     }
 }
