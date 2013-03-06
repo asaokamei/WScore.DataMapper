6f60552a510efc0066dcffd49622cd5e26edd4c1
Asao <kamei@workspot.jp>
2013-03-07 06:45:19 +0900

    made unit test for Selector; tested text and textarea case.

--- tests/WSTests/Selector/Selector_Test.php
+++ tests/WSTests/Selector/Selector_Test.php
@@ -3,53 +3,81 @@ namespace WSTests\Selector;
 
 require( __DIR__ . '/../../autoloader.php' );
 
-/** @var $container \WScore\DiContainer\Container */
-$container = include( __DIR__ . '/../../../vendor/wscore/dicontainer/scripts/instance.php' );
+class Selector_Test extends \PHPUnit_Framework_TestCase
+{
+    /** @var \WScore\Selector\Selector */
+    public $selector;
 
-/** @var $selector \WScore\Selector\Selector */
-$selector = $container->get( '\WScore\Selector\Selector' );
+    function setUp()
+    {
+        /** @var $container \WScore\DiContainer\Container */
+        $container = include( __DIR__ . '/../../../vendor/wscore/dicontainer/scripts/instance.php' );
 
-/**
- * todo: make PHPUnit tests
- */
+        /** @var $selector \WScore\Selector\Selector */
+        $this->selector = $container->get( '\WScore\Selector\Selector' );
 
-// test Selector_Text
+    }
+    function h( $text ) {
+        return htmlspecialchars( $text, ENT_QUOTES, 'UTF-8' );
+    }
+    // +----------------------------------------------------------------------+
+    function test_0()
+    {
+        $selector = $this->selector;
+        $sel = $selector->getInstance( 'text', 'test' );
+        
+        $text = 'text <strong>bold</strong> output';
+        $safe = $this->h( $text );
+        $form = (string) $sel->popHtml( 'form', $text );
+        
+        $this->assertEquals( 'WScore\Selector\Element_Text', get_class( $sel ) );
+        $this->assertEquals( $safe, $sel->popHtml( 'name', $text ) );
+        $this->assertContains( $safe, $form );
+        $this->assertEquals( '<input type="text" name="test" value="text &lt;strong&gt;bold&lt;/strong&gt; output" class="FormSelector" />', $form );
+        
+        // test Selector_Textarea
 
-$sel = $selector->getInstance( 'text', 'test' );
+        $sel = $selector->getInstance( 'textarea', 'test' );
+        $text = "text <strong>bold</strong>\n output";
+        $safe = $this->h( $text );
+        $form = (string) $sel->popHtml( 'form', $text );
 
-echo 'class: ' . get_class( $sel ) . "\n";
-echo 'html: ' . $sel->popHtml( 'name', 'text <strong>bold</strong> output' ) . "\n";
-echo 'form: ' . $sel->popHtml( 'form', 'text <strong>bold</strong> output' ) . "\n";
+        $this->assertEquals( 'WScore\Selector\Element_Textarea', get_class( $sel ) );
+        $this->assertEquals( nl2br( $safe ), $sel->popHtml( 'name', $text ) );
+        $this->assertContains( $safe, $form );
+        $this->assertEquals( 
+            '<textarea name="test" class="FormSelector">text &lt;strong&gt;bold&lt;/strong&gt;'
+                . "\n"
+                . ' output</textarea>', 
+            $form );
+        
+        // test Selector_Hidden
 
-// test Selector_Textarea
+        $sel = $selector->getInstance( 'hidden', 'test' );
 
-$sel = $selector->getInstance( 'textarea', 'test' );
-
-echo 'class: ' . get_class( $sel ) . "\n";
-echo 'html: ' . $sel->popHtml( 'name', "text <strong>bold</strong>\n output" ) . "\n";
-echo 'form: ' . $sel->popHtml( 'form', "text <strong>bold</strong>\n output" ) . "\n";
-
-// test Selector_Hidden
-
-$sel = $selector->getInstance( 'hidden', 'test' );
-
-echo 'class: ' . get_class( $sel ) . "\n";
-echo 'html: ' . $sel->popHtml( 'name', "text <strong>bold</strong>\n output" ) . "\n";
-echo 'form: ' . $sel->popHtml( 'form', "text <strong>bold</strong>\n output" ) . "\n";
+        echo 'class: ' . get_class( $sel ) . "\n";
+        echo 'html: ' . $sel->popHtml( 'name', "text <strong>bold</strong>\n output" ) . "\n";
+        echo 'form: ' . $sel->popHtml( 'form', "text <strong>bold</strong>\n output" ) . "\n";
 
 // test Selector_Mail
 
-$sel = $selector->getInstance( 'mail', 'test' );
+        $sel = $selector->getInstance( 'mail', 'test' );
 
-echo 'class: ' . get_class( $sel ) . "\n";
-echo 'html: ' . $sel->popHtml( 'name', "text <strong>bold</strong>\n output" ) . "\n";
-echo 'form: ' . $sel->popHtml( 'form', "text <strong>bold</strong>\n output" ) . "\n";
+        echo 'class: ' . get_class( $sel ) . "\n";
+        echo 'html: ' . $sel->popHtml( 'name', "text <strong>bold</strong>\n output" ) . "\n";
+        echo 'form: ' . $sel->popHtml( 'form', "text <strong>bold</strong>\n output" ) . "\n";
 
 // test Selector_SelYMD
 
-$sel = $selector->getInstance( 'dateYMD', 'test', 'start_y:1980' );
+        $sel = $selector->getInstance( 'dateYMD', 'test', 'start_y:1980' );
 
-echo 'class: ' . get_class( $sel ) . "\n";
-echo 'html: ' . $sel->popHtml( 'name', "1984-03-31" ) . "\n";
-echo 'form: ' . $sel->popHtml( 'form', "1984-03-31" ) . "\n";
+        echo 'class: ' . get_class( $sel ) . "\n";
+        echo 'html: ' . $sel->popHtml( 'name', "1984-03-31" ) . "\n";
+        echo 'form: ' . $sel->popHtml( 'form', "1984-03-31" ) . "\n";
+
+    }
+}
+
+
+// test Selector_Text
 
