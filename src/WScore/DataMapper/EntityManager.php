d62319cbf7f13be87053ad20e7bd8debebfdfc9a
Asao <kamei@workspot.jp>
2013-03-09 11:35:35 +0900

    added save entities.

--- src/WScore/DataMapper/EntityManager.php
+++ src/WScore/DataMapper/EntityManager.php
@@ -148,4 +148,56 @@ class EntityManager
         }
         return $entity;
     }
+
+    /**
+     * saves or delete registered entities to/from database.
+     */
+    public function save()
+    {
+        if( empty( $this->collection ) ) return;
+        foreach( $this->collection as $entity ) {
+            $this->saveEntity( $entity );
+        }
+    }
+    
+    /**
+     * saves or delete an entity to/from database.
+     *
+     * @param EntityInterface $entity
+     */
+    public function saveEntity( $entity )
+    {
+        $model  = $this->getModel( $entity );
+        if( $entity->toDelete() ) {
+            if( $entity->isIdPermanent() ) { // i.e. entity is from db.
+                $model->delete( $entity->getId() );
+            }
+            // ignore if it is not permanent data; do not have to save. 
+        }
+        elseif( !$entity->isIdPermanent() ) { // i.e. entity is new. insert this.
+            $data = $this->entityToArray( $entity );
+            $id   = $model->insert( $data );
+            $entity->setSystemId( $id );
+        }
+        else {
+            $id   = $entity->getId();
+            $data = $this->entityToArray( $entity );
+            $model->update( $id, $data );
+        }
+    }
+
+    /**
+     * @param EntityInterface $entity
+     * @return array
+     */
+    public function entityToArray( $entity ) {
+        $data = get_object_vars( $entity );
+        foreach( $data as $key => $val ) {
+            if( substr( $key, 0, 1 ) === '_' ) {
+                unset( $data[ $key ] );
+            }
+        }
+        return $data;
+    }
+
 }
