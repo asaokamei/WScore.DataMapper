e47e1593085b212aab2d09bd6a89498e3af6b106
asao Kamei <asao@ubuPC.(none)>
2013-03-13 11:05:20 +0900

    testing BelongsTo relation. same as HasOne's setup.

---
+++ tests/WSTests/DataMapper/Relation/BelongsTo_BasicTest.php
@@ -0,0 +1,114 @@
+<?php
+namespace WSTests\DataMapper\Relation;
+
+use \WSTests\DataMapper\entities\friend;
+
+require( __DIR__ . '/../../../autoloader.php' );
+
+class BelongsTo_BasicTests extends \PHPUnit_Framework_TestCase
+{
+    /** @var mixed */
+    public static $config = 'dsn=mysql:dbname=test_WScore username=admin password=admin';
+
+    public static $table = 'friend';
+
+    /** @var \WScore\DataMapper\EntityManager */
+    public $em;
+
+    public $friendEntity = '\WSTests\DataMapper\entities\friend';
+
+    public $contactEntity = '\WSTests\DataMapper\entities\contact';
+
+    // +----------------------------------------------------------------------+
+    static function setUpBeforeClass()
+    {
+        /** @var $container \WScore\DiContainer\Container */
+        $container = include( __DIR__ . '/../../../../vendor/wscore/dicontainer/scripts/instance.php' );
+        $container->set( '\Pdo', self::$config );
+        /** @var $friend \WSTests\DataMapper\models\Friends */
+        $friend = $container->get( '\WSTests\DataMapper\models\Friends' );
+        $friend->setupTable();
+        /** @var $friend \WSTests\DataMapper\models\Contacts */
+        $contact = $container->get( '\WSTests\DataMapper\models\Contacts' );
+        $contact->setupTable();
+        class_exists( '\WScore\DataMapper\Entity\EntityAbstract' );
+        class_exists( '\WSTests\DataMapper\models\Friends' );
+        class_exists( '\WSTests\DataMapper\entities\friend' );
+    }
+
+    /**
+     * @param int $idx
+     * @return array
+     */
+    function getFriendData( $idx=1 )
+    {
+        /** @var $model \WSTests\DataMapper\models\Friends */
+        $model = $this->em->getModel( $this->friendEntity );
+        return $model->getFriendData( $idx );
+    }
+
+    function getContactData( $idx=1 )
+    {
+        /** @var $model \WSTests\DataMapper\models\Contacts */
+        $model = $this->em->getModel( $this->contactEntity );
+        return $model->makeContact( $idx );
+    }
+
+    /**
+     *
+     */
+    function setUp()
+    {
+        /** @var $container \WScore\DiContainer\Container */
+        $container = include( __DIR__ . '/../../../../vendor/wscore/dicontainer/scripts/instance.php' );
+        $container->set( 'ContainerInterface', $container );
+        $container->set( '\Pdo', self::$config );
+        // set up persistence model
+        $this->em = $container->get( '\WScore\DataMapper\EntityManager' );
+    }
+
+    // +----------------------------------------------------------------------+
+    function test_new_entity_and_insert()
+    {
+        $em = $this->em;
+        $friend  = $em->newEntity( $this->friendEntity,  $this->getFriendData(1) );
+        $contact1 = $em->newEntity( $this->contactEntity, $this->getContactData(1) );
+        $contact2 = $em->newEntity( $this->contactEntity, $this->getContactData(2) );
+        $em->save();
+        $relation = $em->relation( $friend, 'contacts' );
+
+        // basic check.
+        $this->assertEquals( 'WScore\DataMapper\Relation\BelongsTo', get_class( $relation ) );
+
+        $relation->set( $contact1 );
+        $relation->set( $contact2 );
+        $this->assertEquals( $friend[ 'friend_id' ], $contact1[ 'friend_id' ] );
+        $this->assertEquals( $friend[ 'friend_id' ], $contact2[ 'friend_id' ] );
+
+        $em->save();
+    }
+    function test_fetch_related_entities()
+    {
+        $em = $this->em;
+        $friend   = $em->fetch( $this->friendEntity,  '1' );
+        $friend   = $friend[0];
+        // get related contacts.
+        $relation = $em->relation( $friend, 'contacts' );
+        $contacts = $relation->fetch();
+        // test contacts are array and have 2 contacts from previous test.
+        $this->asserttrue( is_array( $contacts ) );
+        $this->assertEquals( 2, count( $contacts ) );
+        // and these contact's friend_id is the friend_id.
+        $this->assertEquals( $friend[ 'friend_id' ], $contacts[0][ 'friend_id' ] );
+        $this->assertEquals( $friend[ 'friend_id' ], $contacts[1][ 'friend_id' ] );
+
+        // now test set.
+        $contact3 = $em->newEntity( $this->contactEntity, $this->getContactData(3) );
+        $relation->set( $contact3 );
+        $em->save();
+        $contacts = $em->relation( $friend, 'contacts' )->fetch();
+        // test contacts are array and have 2 contacts from previous test.
+        $this->asserttrue( is_array( $contacts ) );
+        $this->assertEquals( 3, count( $contacts ) );
+    }
+}
