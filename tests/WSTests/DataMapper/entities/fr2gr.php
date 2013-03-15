f7fb01bf21530850b27bad4773ef6d81502f6030
Asao <kamei@workspot.jp>
2013-03-16 06:15:16 +0900

    join entity structure.

--- tests/WSTests/DataMapper/entities/fr2gr.php
+++ tests/WSTests/DataMapper/entities/fr2gr.php
@@ -6,10 +6,16 @@ use \WScore\DataMapper\Entity\EntityAbstract;
 class fr2gr extends EntityAbstract
 {
     static $_modelName = '\WSTests\DataMapper\models\Fr2Gr';
-    public function getLeft() {
+    public $fr2gr_id;
+    public $friend_id;
+    public $contact_id;
+    public $created_at;
+    public $updated_at;
+    
+    public static function getLeft() {
         return 'friend_id';
     }
-    public function getRight() {
+    public static function getRight() {
         return 'contact_id';
     }
 }
