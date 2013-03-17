cf486656ade9d5a3316bc6de6fddbbfd462ccf08
Asao <kamei@workspot.jp>
2013-03-17 10:14:52 +0900

    slightly different test by setting new entities to a saved entity.

--- tests/WSTests/DataMapper/Relation/JoinBy_SetManyTest.php
+++ tests/WSTests/DataMapper/Relation/JoinBy_SetManyTest.php
@@ -11,13 +11,14 @@ class JoinBy_SetManyTest extends JoinBySetUp
     {
         $em = $this->em;
         $friend = $em->newEntity( $this->friendEntity, $this->getFriendData(1) );
+        $em->save();
         $group1 = $em->newEntity( $this->groupEntity,  $this->getGroupData(0) );
         $group2 = $em->newEntity( $this->groupEntity,  $this->getGroupData(1) );
         $group3 = $em->newEntity( $this->groupEntity,  $this->getGroupData(2) );
         $group4 = $em->newEntity( $this->groupEntity,  $this->getGroupData(3) );
+        $groups = array( $group1, $group3 );
         $relation = $em->relation( $friend, 'groups' );
-        $relation->add( $group1 );
-        $relation->add( $group3 );
+        $relation->set( $groups );
         $em->save();
         $this->assertFalse( $relation->isLinked() );
         $relation->link();
@@ -45,5 +46,17 @@ class JoinBy_SetManyTest extends JoinBySetUp
         $groups = array( $group2[0], $group4[0] );
         $relation->set( $groups );
         $em->save();
+    }
+    function test_fetch_new_relation()
+    {
+        $em = $this->em;
+        $friend   = $em->fetch( $this->friendEntity,  '1' );
+        /** @var $friend \WSTests\DataMapper\entities\friend */
+        $friend   = $friend[0];
+        $relation = $em->relation( $friend, 'groups' );
+        $groups = $relation->fetch();
+        $this->assertEquals( 2, count( $groups ) );
+        $this->assertTrue( in_array( $groups[0]['group_code'], array( 'test', 'more3' ) ) );
+        $this->assertTrue( in_array( $groups[1]['group_code'], array( 'test', 'more3' ) ) );
     }
 }
