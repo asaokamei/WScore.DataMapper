d72f7f4c1cae69677e94f40bb8f3b6b256a1229c
Asao <kamei@workspot.jp>
2013-07-14 06:35:04 +0900

    add FilterAbstract for filters.

---
+++ src/WScore/DataMapper/Model/FilterAbstract.php
@@ -0,0 +1,56 @@
+<?php
+namespace WScore\DataMapper\Model;
+
+abstract class FilterAbstract implements FilterInterface
+{
+    /**
+     * @var Model
+     */
+    public $model;
+
+    /**
+     * @param \WScore\DataMapper\Entity\EntityInterface $entity
+     */
+    public function onSave( $entity )
+    {
+    }
+
+    /**
+     * @param \WScore\DbAccess\Query $query
+     */
+    public function onQuery( $query )
+    {
+        
+    }
+    
+    /**
+     * @param \WScore\DbAccess\Query $query
+     */
+    public function onFetch( $query )
+    {
+        
+    }
+
+    /**
+     * @param \WScore\DataMapper\Entity\EntityInterface $entity
+     */
+    public function onApply( $entity )
+    {
+        
+    }
+
+    /**
+     * @param \WScore\DataMapper\Entity\EntityInterface $entity
+     */
+    public function onValidate( $entity )
+    {
+        
+    }
+    /**
+     * @param Model $model
+     */
+    public function setModel( $model )
+    {
+        $this->model = $model;
+    }
+}
