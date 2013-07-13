99e90cefbfca8f5f72d4f260e71e5b993060f0e2
Asao <kamei@workspot.jp>
2013-07-14 06:35:24 +0900

    maybe add more filter events.

--- src/WScore/DataMapper/Model/FilterInterface.php
+++ src/WScore/DataMapper/Model/FilterInterface.php
@@ -3,8 +3,10 @@ namespace WScore\DataMapper\Model;
 
 interface FilterInterface
 {
-    public function onSave( $query );
-    public function onFetch( $entity );
+    public function onSave(  $entity );
+    public function onQuery( $query );
+    public function onFetch( $query );
     public function onApply( $entity );
     public function onValidate( $entity );
+    public function setModel( $model );
 }
