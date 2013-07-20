4582bd79fec9afe7365c0af3f587dc09996a2a14
Asao <kamei@workspot.jp>
2013-07-21 06:01:07 +0900

    create Paginate filter. to be used in model or entity manager.

---
+++ src/WScore/DataMapper/Filter/Paginate.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace WScore\DataMapper\Filter;
+
+/**
+ * Class Paginate
+ *
+ * @package WScore\DataMapper\Filter
+ * 
+ * @method setOptions
+ */
+class Paginate implements FilterInterface
+{
+    /**
+     * @Inject
+     * @var \WScore\DbAccess\Tools\Paginate
+     */
+    public $paginate;
+
+    /**
+     * @param string $name
+     * @param mixed  $value
+     * @return $this
+     */
+    public function set( $name, $value ) {
+        $this->paginate->$name = $value;
+        return $this;
+    }
+
+    /**
+     * @param $query
+     * @return mixed
+     */
+    public function onQuery( $query )
+    {
+        $this->paginate->setQuery( $query );
+        return $query;
+    }
+
+    /**
+     * @param $name
+     * @param $args
+     * @return $this|mixed|Paginate
+     */
+    public function __call( $name, $args ) 
+    {
+        $returned = $this;
+        if( method_exists( $this->paginate, $name ) ) {
+            $returned = call_user_func_array( array( $this->paginate, $name ), $args );
+        }
+        if( $returned instanceof \WScore\DbAccess\Tools\Paginate ) {
+            return $this;
+        }
+        return $returned;
+    }
+}
